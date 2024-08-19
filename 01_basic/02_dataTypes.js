0470e131f23c3f229dfe59ca9309cf40895074fd
JunaidAkram86 <junaid.sahil@gmail.com>
2024-08-19 21:33:28 +0000

    Datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+ let name = "Junaid" // strings
+ let age = 18 // number
+ let isloggedIn = false //boolean
+ let state; //undefined
+  
+ // datatypes are number,bigint,string,boolean,null(standalone value),undefined, symbol (to find uniquesss) 
+
+
+ //Object
+ console.log(typeof undefined); //undefined
+ console.log(typeof null); //object
+ 
