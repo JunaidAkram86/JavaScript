d07d37b46a287ab79503d0dfb30ad5a2c7ca7978
JunaidAkram86 <junaid.sahil@gmail.com>
2024-08-20 20:53:38 +0000

    Operations

--- 01_basic/03_conversionoperations.js
+++ 01_basic/03_conversionoperations.js
@@ -21,5 +21,32 @@ let booleanIsLoggedIn = Boolean (isLoggedIn)
 
 let someNumber = 33 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+// ********************************Operations ****************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+let str1 = "Hello"
+let str2 = " Junaid"
+let str3 = str1 + str2
+//console.log(str3)
+//console.log(+true); // print 1
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter); // print 101
+
+
+
+
+
+
+
